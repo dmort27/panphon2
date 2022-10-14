b26c690adf970f2f889cf8b555f486f780538850
David R. Mortensen <dmortens@cs.cmu.edu>
2022-10-14 11:09:21 -0400

    Added comments

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,11 @@ struct FeatureTable {
 
 #[pymethods]
 impl FeatureTable {
+    //! The `FeatureTable` class implements the loading, extracting, and
+    //! analysis of representations based on phonological (typically
+    //! articulatory) features. It constructs a FeatureTable. Currently, the
+    //! Python interface only supports the internal table that is compiled into
+    //! the rust libraray, though adding support for arbitrary tables is planned.
     #[new]
     fn new() -> Self {
         FeatureTable {
@@ -15,18 +20,24 @@ impl FeatureTable {
         }
     }
 
+    /// Returns the sequence of phonemes present in a Unicode IPA string, using
+    /// the phoneme vocabulary from the FeatureTable.
     fn phonemes(&self, s: &str) -> Vec<String> {
         self.ft.phonemes(s)
     }
 
+    /// Converts a list of phonemes into a list of lists of feature specifications.
     fn phonemes_to_vectors(&self, ps: Vec<String>) -> Vec<Vec<i8>> {
         self.ft.phonemes_to_vectors(ps)
     }
 
+    /// Converts a word, in Unicode IPA, into a list of lists of feature specifications.
     fn word_to_vectors(&self, s: &str) -> Vec<Vec<i8>> {
         self.ft.phonemes_to_vectors(self.ft.phonemes(s))
     }
 
+    /// Converts a word, in Unicode IPA, into a list of lists of binary features
+    /// (with 0 and -1 collapsed to 0).
     fn word_to_binary_vectors(&self, s: &str) -> Vec<Vec<i8>> {
         self.ft
             .phonemes_to_vectors(self.ft.phonemes(s))
@@ -35,6 +46,8 @@ impl FeatureTable {
             .collect::<Vec<Vec<i8>>>()
     }
 
+    /// Performs sum pooling on the feature vectures extracted from a word and
+    /// returns the resulting matrix.
     fn word_to_bag_of_features(&self, s: &str) -> Vec<i8> {
         let mut tensor = self
             .ft
@@ -47,10 +60,11 @@ impl FeatureTable {
                 acc.iter().zip(v).map(|(f1, f2)| f1 + f2).collect()
             })
         } else {
-            panic!("Empty tensor");
+            panic!("Empty matrix");
         }
     }
 
+    /// Returns the unweighted feature distance between to words.
     fn feature_edit_distance(&self, s1: &str, s2: &str) -> f64 {
         self.ft.feature_edit_distance(s1, s2)
     }
